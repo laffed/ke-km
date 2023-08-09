ff3b4059b2c291301e5b19b28cac50529dee4b34
Alex Stepanov <alvicstep@gmail.com>
2023-08-09 13:43:58 -0700

    Added compiled firmware

--- keymap.c
+++ keymap.c
@@ -1,19 +1,3 @@
-/* Copyright 2023 Alex Stepanov
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
 enum layer_names {
@@ -45,7 +29,7 @@ enum layer_names {
 */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE] = LAYOUT_pretty(
+[_BASE] = LAYOUT(
 KC_ESC,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,                 KC_F9,KC_F10,KC_F11,KC_F12,KC_PSCR,KC_SCRL,KC_PAUS,KC_NO,QK_BOOT,
 KC_EQL,KC_1,KC_2,KC_3,KC_4,KC_5,                                       KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINS,
 KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,                                        KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLS,
@@ -77,7 +61,7 @@ KC_NO,KC_GRV,KC_9,KC_0,                                                     KC_L
 *                                 |      |       | Ctrl |         | Ctrl |      |      |
 *                                 `-------------- ------'         `--------------------'
 */
-[_KEYPAD] = LAYOUT_pretty(
+[_KEYPAD] = LAYOUT(
  KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,                 KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,
  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,                          KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
  KC_TRNS,KC_TRNS,KC_TRNS,KC_9,KC_0,KC_TRNS,                                   KC_TRNS,KC_MINS,KC_BSLS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -108,7 +92,7 @@ KC_NO,KC_GRV,KC_9,KC_0,                                                     KC_L
 *                                 `--------------------'         `--------------------'
 */
 
-[_NUMPAD] = LAYOUT_pretty(
+[_NUMPAD] = LAYOUT(
  KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,                 KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,
  KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,                                   KC_NO,KC_NO,KC_EQL,KC_SLSH,KC_ASTR,KC_NO,
  KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,                                   KC_NO,KC_7,KC_8,KC_9,KC_MINS,KC_NO,
